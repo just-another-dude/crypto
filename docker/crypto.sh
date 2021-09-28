94af1d25b9ebdcb50d393099dcd176ad74d45768
just-another-dude <49374459+just-another-dude@users.noreply.github.com>
2021-09-28 19:35:01 +0300

    Create a requirements.txt file

--- docker/crypto.sh
+++ docker/crypto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 /usr/local/bin/pip install jupyter requests python-coinmarketcap pandas
+# Create a requirements.txt file, then install the packages according to it.
 /usr/local/bin/jupyter-notebook --port=8080 --ip=0.0.0.0 --NotebookApp.token='' --allow-root
 
